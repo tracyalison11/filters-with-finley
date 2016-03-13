a51c4184286a58cd10cd7b5eca62d31a29d8d522
tracyalison11 <tracyak13@gmail.com>
2016-03-13 15:07:00 -0700

    commit files

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+angular.module('filtersWithFinley', [])
+
+    .controller('finleyController', function($scope) {
+
+        $scope.finleys = [{
+            type: 'puppy',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_puppy2_zpsarpl2uad.jpg'
+        }, {
+            type: 'puppy',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_puppy_zpsav4un7ch.jpg'
+        }, {
+            type: 'puppy',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_puppy3_zpseyuyoikv.jpg'
+        }, {
+            type: 'teen',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_teen_zpsj8uxqouu.jpg'
+        }, {
+            type: 'teen',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_teen2_zpsoft9gj07.jpg'
+        }, {
+            type: 'teen',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_teen3_zps4qjvo293.jpg'
+        }, {
+            type: 'dog',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_dog3_zpspdhaokcf.jpg'
+        }, {
+            type: 'dog',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_dog2_zpss0eangjf.jpg'
+        }, {
+            type: 'dog',
+            imageUrl: 'http://img.photobucket.com/albums/v433/Tracetrace/finley_dog_zpsuuzp9llp.jpg'
+        }];
+
+    });
+
